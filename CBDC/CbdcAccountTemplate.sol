11ea1a0d12330f6feb82fa2c87326a6e448290f5
will@solv.finance <will@solv.finance>
2022-09-05 14:20:34 +0800

    fix typos

--- CBDC/CbdcAccountTemplate.sol
+++ CBDC/CbdcAccountTemplate.sol
@@ -9,11 +9,11 @@ contract CbdcAccountTemplate is ERC20 {
     uint256 private amountTransferredInlast24Hours;
     uint256 private balance;
     
-    function transfer(uint256 amount)) returns (bool)
+    function transfer(uint256 amount) returns (bool)
     {
         require(amount + amountTransferredInlast24Hours) < 50,000.00;
-
     }
+
     function receive(uint256 amount) returns (bool)
     {
         require(amount + balance) < 5,000,000.00;
